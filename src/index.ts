f0d7658c43fa075ab02ebb3c47c8a2c901322629
kiki-kanri <a470666@gmail.com>
2024-11-10 11:51:25 +0800

    feat: add entry point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-(() => console.log('Hello world!'))();
+import { sequelize } from '@/constants';
+import '@/models';
+
+(async () => {
+	if (process.getuid?.() !== 0) {
+		console.error('This program must be run as root.');
+		process.exit(1);
+	}
+
+	await sequelize.sync();
+})();
